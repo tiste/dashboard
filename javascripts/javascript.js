a8aaa21312a0ff86a569ee03eeca8be6d486d75b
Baptiste Lecocq <baptiste.lecocq@gmail.com>
2014-04-01 10:11:42 +0200

    Automated commit at 2014-04-01 08:11:42 UTC by middleman-deploy 0.2.3

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,1 +1,1 @@
-(function(){this.openUrl=function(n){return window.open(n,"_self")}}).call(this);
+(function(){this.goTo=function(e){return window.open(e,"_self")},this.isChar=function(e){return e.keyCode>47&&e.keyCode<58||32===e.keyCode||e.keyCode>64&&e.keyCode<91||e.keyCode>95&&e.keyCode<112||e.keyCode>185&&e.keyCode<193||e.keyCode>218&&e.keyCode<223}}).call(this);
